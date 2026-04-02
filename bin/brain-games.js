00b6b29af0ea842696686664f436263e6168847f
programersha <khrapinaiuliy188@gmail.com>
2026-04-02 22:09:00 +0300

    add getNameAndSayHello function

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,3 +1,6 @@
 #!/usr/bin/env node
+import getNameAndSayHello from '../src/cli.js'
 
-console.log("Welcome to the Brain Games!");
+console.log('Welcome to the Brain Games!')
+
+getNameAndSayHello()
