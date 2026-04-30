be59ff0b40e4aa8df08412e0ab847ed0c6d3c21a
programersha <khrapinaiuliy188@gmail.com>
2026-04-30 11:43:26 +0300

    add brain-prime game

---
+++ src/games/prime.js
@@ -0,0 +1,39 @@
+import { gameBase, generateNumber } from '../index.js'
+
+const primeGame = () => {
+  const gameRules = 'Answer "yes" if given number is prime. Otherwise answer "no".'
+  gameBase(gameRules, getQandA)
+}
+
+const getQandA = () => {
+  const num = generateNumber()
+
+  return {
+    question: num,
+    answer: isPrime(num) ? 'yes' : 'no',
+  }
+}
+
+const isPrime = (num) => {
+  if (num < 2) {
+    return false
+  }
+
+  if (num === 2) {
+    return true
+  }
+
+  if (num % 2 === 0) {
+    return false
+  }
+
+  for (let i = 3; i * i <= num; i += 2) {
+    if (num % i === 0) {
+      return false
+    }
+  }
+
+  return true
+}
+
+export default primeGame
