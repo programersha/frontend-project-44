be2695b7319ac069690d5a9e9f0a80bb715be652
programersha <khrapinaiuliy188@gmail.com>
2026-07-25 16:35:41 +0300

    refactor: moved the general game logic into separate functions

--- src/games/calc.js
+++ src/games/calc.js
@@ -9,27 +9,31 @@ const getQandA = () => {
   const firstNum = getRandomIntInclusive(0, 100)
   const secondNum = getRandomIntInclusive(0, 100)
   const operator = generateOperator()
-  let question = ''
-  let answer = 0
-
-  switch (operator) {
-    case '+':
-      question += `${firstNum} + ${secondNum}`
-      answer += firstNum + secondNum
-      break
-    case '-':
-      question += `${firstNum} - ${secondNum}`
-      answer += firstNum - secondNum
-      break
-    case '*':
-      question += `${firstNum} * ${secondNum}`
-      answer += firstNum * secondNum
-      break
-  }
-
+  const { question, answer } = calc(firstNum, secondNum, operator)
+  
   return {
     question: question,
     answer: `${answer}`,
+  }
+}
+
+const calc = (firstNum, secondNum, operator) => {
+  switch (operator) {
+    case '+':
+      return {
+        question: `${firstNum} + ${secondNum}`,
+        answer: firstNum + secondNum
+      }
+    case '-':
+      return {
+        question: `${firstNum} - ${secondNum}`,
+        answer: firstNum - secondNum
+      }
+    case '*':
+      return {
+      question: `${firstNum} * ${secondNum}`,
+      answer: firstNum * secondNum
+      }
   }
 }
 
